a2a0d3febaac81640039ef8361a7815b1ba0deb5
Vidarshan <vidarshanadithya3@gmail.com>
2021-12-01 19:00:55 +0530

    Added footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,28 @@
+import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
+
 export default function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="grid grid-cols-2 bg-black p-10 text-gray-300">
+      <div className="flex flex-col">
+        <div className="py-2">Find my Stay</div>
+        <div className="py-2 text-xs">2022 @ findmystay.com</div>
+        <div className="py-2 text-xs">Terms and Conditions</div>
+        <div className="py-2 text-xs">About</div>
+      </div>
+      <div className="flex flex-col">
+        <div className="p-2">Get notified when we do something special!</div>
+        <div className="flex flex-row">
+          <div class="p-2">
+            <BsInstagram />
+          </div>
+          <div class="p-2">
+            <BsTwitter />
+          </div>
+          <div class="p-2">
+            <BsFacebook />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
