1445d47486eb9dd95ca05cc6b505b7fda10cf17e
Vidarshan <vidarshanadithya3@gmail.com>
2021-12-04 14:14:27 +0530

    Fetch hotels

---
+++ pages/api/hotels.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  try {
+    const hotels = require("../api/data.json");
+    res.status(200).json(hotels);
+  } catch (err) {
+    res.status(400).json({ message: "An error occurred" });
+  }
+}
